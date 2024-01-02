cc1ce995deb87e8e2fb8daf950a2e688da5a00db
Megs-Kunneke <megs@kunneke.co.za>
2024-01-02 14:19:19 +0200

    Devise Login/Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
+  
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
 
   root 'dashboard#index'
   resources :spec_checks
